a7a0ff7790c3a57a2d2b363429b0da24e3e20bf7
Mandy1125 <mandeep1983.s@gmail.com>
2016-04-14 15:18:50 +0530

    First update

---
+++ app/src/main/java/in/foodtalk/android/FbLogin.java
@@ -0,0 +1,107 @@
+package in.foodtalk.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FbLogin extends AppCompatActivity implements OnClickListener {
+
+
+    private CallbackManager callbackManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fb_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(this);
+        loginButton.setReadPermissions("public_profile", "email", "user_friends");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+               // System.out.print("Logged in");
+                String userId = loginResult.getAccessToken().getUserId();
+                Log.d("facebook","Loged in: "+ loginResult);
+
+                //-----graph api---------facebook-------
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code.
+                                try {
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday"); // 01/31/1980 format
+                                    String name = object.getString("name");
+                                    String gender = object.getString("gender");
+                                    Log.d("fb user info", "name: "+ name + " email: "+ email+" gender: "+ gender+" birthday: "+ birthday);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+                //-------------
+
+                //--Start new activity--
+                Intent i = new Intent(FbLogin.this, WelcomeUsername.class);
+                startActivity(i);
+                finish();
+                //----------------------
+            }
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("facebook","oncancel");
+            }
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.i("Error" , "Error");
+            }
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                // OK button
+                Log.d("faceboo: ","click login button");
+                break;
+        }
+    }
+}
