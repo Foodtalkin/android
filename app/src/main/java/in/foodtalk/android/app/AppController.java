9bf4ed2bd67c4cb40fd1fc482c5dd918d9581b38
Mandy1125 <mandeep1983.s@gmail.com>
2016-09-28 13:13:14 +0530

    working on newPostFlow bk

--- app/src/main/java/in/foodtalk/android/app/AppController.java
+++ app/src/main/java/in/foodtalk/android/app/AppController.java
@@ -127,7 +127,6 @@ public class AppController extends Application {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(getApplicationContext());
         }
-
         return mRequestQueue;
     }
 
